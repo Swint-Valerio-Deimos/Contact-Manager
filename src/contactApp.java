f62fe07aa34b650e44f56750b53240c622fc08cb
andrewswint <andrewswint1976@gmail.com>
2019-12-09 11:43:45 -0600

    basic framework, creating lists and path

--- src/contactApp.java
+++ src/contactApp.java
@@ -1,7 +1,44 @@
+import java.nio.file.Path;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+
+
 public class contactApp {
 
+    final static String folder = "data";
+    final static String fileName = "contacts.txt";
+
+    static List<String> contacts = Arrays.asList();
+
     public static void main(String[] args) {
-        System.out.println("test");
+
+        Path dataDirectory = Paths.get(folder);
+        Path dataFile = Paths.get(folder, fileName);
+
+        try {
+            if (Files.notExists(dataDirectory)) {
+                Files.createDirectories(dataDirectory);
+            }
+
+            if (!Files.exists(dataFile)) {
+                Files.createFile(dataFile);
+            }
+
+            Path filepath = Paths.get("data", "contacts.txt");
+            Files.write(filepath, contacts);
+
+
+        } catch (IOException x) {
+            x.printStackTrace();
+        }
+
     }
 
 }
