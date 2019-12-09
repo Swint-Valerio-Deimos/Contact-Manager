8258ad91be133fdca31c065b56d06045614ba90a
andrewswint <andrewswint1976@gmail.com>
2019-12-09 12:41:16 -0600

    basic construction done

---
+++ src/contact.java
@@ -0,0 +1,27 @@
+public class contact {
+
+    private String name;
+    private String phoneNumber;
+
+    public contact(String name, String number) {
+        this.name = name;
+        this.phoneNumber = number;
+    }
+
+    public void setName (String name){
+        this.name = name;
+    }
+
+    public void setPhoneNumber (String number){
+        this.phoneNumber = number;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+}
--- src/contactApp.java
+++ src/contactApp.java
@@ -1,13 +1,9 @@
 import java.nio.file.Path;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-
+import java.util.*;
 
 
 public class contactApp {
@@ -15,13 +11,21 @@ public class contactApp {
     final static String folder = "data";
     final static String fileName = "contacts.txt";
 
-    static List<String> contacts = Arrays.asList();
+    //    make the list
+    static List<String> contacts = new ArrayList<>();
+
+    static contact contact = new contact("test", "test");
+
+//    Scanner
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
 
+        //        Creates the path
         Path dataDirectory = Paths.get(folder);
         Path dataFile = Paths.get(folder, fileName);
 
+        //        creates file and directory if one or both dont already exist.
         try {
             if (Files.notExists(dataDirectory)) {
                 Files.createDirectories(dataDirectory);
@@ -32,7 +36,16 @@ public class contactApp {
             }
 
             Path filepath = Paths.get("data", "contacts.txt");
+
             Files.write(filepath, contacts);
+
+            //            adds line to the list.
+            Files.write(
+                    Paths.get("data", "contacts.txt"),
+                    Arrays.asList("info: " + contact.getName() + " | " + contact.getPhoneNumber()),
+                    StandardOpenOption.APPEND
+            );
+
 
 
         } catch (IOException x) {
